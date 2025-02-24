0276db90148f162fbadb18229fc60700d82287af
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-02-24 10:40:22 +0200

    Solution for problem N.

---
+++ Day 3/Problems_L_U/N/N.cs
@@ -0,0 +1,11 @@
+﻿namespace N;
+
+class N
+{
+    static void Main(string[] args)
+    {
+        char letter = char.Parse(Console.ReadLine());
+
+        Console.WriteLine((letter >= 'a') ? letter.ToString().ToUpper() : letter.ToString().ToLower());
+    }
+}
