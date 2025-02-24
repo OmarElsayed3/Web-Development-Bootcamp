9f33720fd3dec8e47ba5c67ecd69e377567da5f1
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-02-24 10:47:32 +0200

    Solution for problem Q.

---
+++ Day 3/Problems_L_U/Q/Q.cs
@@ -0,0 +1,21 @@
+﻿namespace Q;
+
+class Q
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        
+        double x = double.Parse(input[0]);
+        double y = double.Parse(input[1]);
+        
+        if (y == 0 && x != 0)
+            Console.WriteLine("Eixo X");
+        else if (x == 0 && y != 0)
+            Console.WriteLine("Eixo Y");
+        else if (x == 0 && y == 0)
+            Console.WriteLine("Origem");
+        else
+            Console.WriteLine((x > 0) ? ((y > 0) ? "Q1" : "Q4") : ((y > 0) ? "Q2" : "Q3"));
+    }
+}
