b17b56be085b8f0bb08cc45200a1ac6cc733ee13
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-02-24 10:43:26 +0200

    Solution for problem O.

---
+++ Day 3/Problems_L_U/O/O.cs
@@ -0,0 +1,40 @@
+﻿namespace O;
+
+class O
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        char operation = ' ';
+        foreach (var value in input)
+        {
+            if (value < '0')
+            {
+                operation = value;
+                break;
+            }
+        }
+        
+        string[] nums = input.Split(operation);
+        int num1 = int.Parse(nums[0]);
+        int num2 = int.Parse(nums[1]);
+        
+        switch (operation)
+        {
+            case '+':
+                Console.WriteLine(num1 + num2);
+                break;
+            case '-':
+                Console.WriteLine(num1 - num2);
+                break;
+            case '*':
+                Console.WriteLine(num1 * num2);
+                break;
+            case '/':
+                Console.WriteLine(num1 / num2);
+                break;
+            
+            
+        }
+    }
+}
