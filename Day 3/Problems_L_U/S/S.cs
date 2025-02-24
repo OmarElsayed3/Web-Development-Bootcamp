2d76227dfea1805e6f51ad09a548b53c4fe8d5a8
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-02-24 10:51:10 +0200

    Solution for problem S.

---
+++ Day 3/Problems_L_U/S/S.cs
@@ -0,0 +1,20 @@
+﻿namespace S;
+
+class S
+{
+    static void Main(string[] args)
+    {
+        float x = float.Parse(Console.ReadLine());
+        
+        if (x >= 0 && x <= 25)
+            Console.WriteLine("Interval [0,25]");
+        else if (x > 25 && x <= 50)
+            Console.WriteLine("Interval (25,50]");
+        else if (x > 50 && x <= 75)
+            Console.WriteLine("Interval (50,75]");
+        else if (x > 75 && x <= 100)
+            Console.WriteLine("Interval (75,100]");
+        else
+            Console.WriteLine("Out of Intervals");
+    }
+}
