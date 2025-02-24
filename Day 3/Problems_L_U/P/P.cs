0da92c3c3ad1e861ac4b55aeeda81b2f6699780a
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-02-24 10:46:01 +0200

    Solution for problem P.

---
+++ Day 3/Problems_L_U/P/P.cs
@@ -0,0 +1,12 @@
+﻿namespace P;
+
+class P
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        int digit = (int)input[0];
+        Console.WriteLine((digit % 2 == 0) ? "EVEN" : "ODD");
+    }
+}
