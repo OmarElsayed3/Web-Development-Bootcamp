cd35a7e0119c272f88ce40785a1aedde48dc0aa5
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-02-26 20:16:33 +0200

    Problem W on Sheet1

---
+++ Day 4/Codeforces Task/Sheet1_T_To_Z/W/W.cs
@@ -0,0 +1,29 @@
+﻿namespace W;
+
+class W
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int num1 = int.Parse(input[0]);
+        char ch = char.Parse(input[1]);
+        int num2 = int.Parse(input[2]);
+        char ch2 = char.Parse(input[3]);
+        int num3 = int.Parse(input[4]);
+        switch (ch)
+        {
+            case '+':
+                Console.WriteLine(num1 + num2 == num3 ? "Yes" : num1 + num2);   
+                break;
+            case '-':
+                Console.WriteLine(num1 - num2 == num3 ? "Yes" : num1 - num2);   
+                break;
+            case '*':
+                Console.WriteLine(num1 * num2 == num3 ? "Yes" : num1 * num2);   
+
+                break;
+            
+        }
+
+    }
+}
