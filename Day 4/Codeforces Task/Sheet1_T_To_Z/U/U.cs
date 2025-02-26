b84914163eee4f6735d588cec6ce3f97f9c5a21e
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-02-26 20:11:01 +0200

    Problem U on Sheet1

---
+++ Day 4/Codeforces Task/Sheet1_T_To_Z/U/U.cs
@@ -0,0 +1,15 @@
+﻿namespace U;
+
+class U
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        float num = float.Parse(input[0]);
+        
+        int num2 = (int)num;
+
+        Console.WriteLine((num == num2) ? $"int {num}" : $"float {num2} {num - num2}");
+
+    }
+}
