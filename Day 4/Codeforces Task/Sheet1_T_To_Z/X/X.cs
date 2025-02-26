ad4cb7f2ff492ca33f78f946022ebed6cc2fce45
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-02-26 20:17:51 +0200

    Problem X on Sheet1

---
+++ Day 4/Codeforces Task/Sheet1_T_To_Z/X/X.cs
@@ -0,0 +1,18 @@
+﻿namespace X;
+
+class X
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int l1 = int.Parse(input[0]);
+        int r1 = int.Parse(input[1]);
+        int l2 = int.Parse(input[2]);
+        int r2 = int.Parse(input[3]);
+        
+        int left=Math.Max(l1, l2);
+        int right=Math.Min(r1, r2);
+
+        Console.WriteLine(left > right ? -1 : $"{left} {right}");
+    }
+}
