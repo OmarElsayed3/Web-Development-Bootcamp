db58c8529b6f754892b19297455ba5b080fd6a95
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-04-14 16:18:15 +0200

    Sign Up project code.

--- Sign-up/src/singup.js
+++ Sign-up/src/singup.js
@@ -1,21 +1,21 @@
-function passwordVisibility(ID: string): void {
-    const inputField = document.getElementById(ID) as HTMLInputElement | null;
+function passwordVisibility(ID) {
+    var _a;
+    var inputField = document.getElementById(ID);
     if (!inputField) {
-        console.error(`Element with id "${ID}" not found.`);
+        console.error("Element with id \"".concat(ID, "\" not found."));
         return;
     }
-
-    const eyeIcon = inputField.nextElementSibling?.querySelector('i');
+    var eyeIcon = (_a = inputField.nextElementSibling) === null || _a === void 0 ? void 0 : _a.querySelector('i');
     if (!eyeIcon) {
         console.error("Eye icon not found.");
         return;
     }
-
     if (inputField.type === "password") {
         inputField.type = "text";
         eyeIcon.classList.remove('fa-eye-slash');
         eyeIcon.classList.add('fa-eye');
-    } else {
+    }
+    else {
         inputField.type = "password";
         eyeIcon.classList.remove('fa-eye');
         eyeIcon.classList.add('fa-eye-slash');
