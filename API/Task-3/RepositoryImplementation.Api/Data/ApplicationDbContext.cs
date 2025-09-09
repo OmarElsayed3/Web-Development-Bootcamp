6bec68b4ddc43409be3f72cfc4ba8b0521d8f61a
Omar <omar.el.sayed.mahmoud1@gmail.com>
2025-09-09 16:20:05 +0300

    chore: create initial project directories

---
+++ API/Task-3/RepositoryImplementation.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_3.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
